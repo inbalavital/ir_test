d45d74db09e00cbe34cb3cade9806cea5fd900b6
iifrach <iifrach@paloaltonetworks.com>
2025-03-25 15:33:11 +0200

    Add many files

--- CKV_AWS_163.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_ecr_repository" "foo" {
-  name                 = "bar"
-  image_tag_mutability = "MUTABLE"
-}
--- CKV_GCP_14.tf
+++
@@ -1,8 +0,0 @@
-resource google_sql_database_instance "db_instance" {
-  settings {
-    backup_configuration {
-      enabled = false
-    }
-    tier = "STANDARD"
-  }
-}
