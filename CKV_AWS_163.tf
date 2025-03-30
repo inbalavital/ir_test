f6f1cafd406654f57f6ff82502494ada2c64756c
iifrach <iifrach@paloaltonetworks.com>
2025-03-30 13:46:48 +0300

    Add resource

--- CKV_AWS_163.tf
+++ CKV_AWS_163.tf
@@ -2,3 +2,12 @@ resource "aws_ecr_repository" "foo" {
   name                 = "bar"
   image_tag_mutability = "MUTABLE"
 }
+
+resource "aws_ebs_volume" "example" {
+  availability_zone = "us-west-2a"
+  size              = 40
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
